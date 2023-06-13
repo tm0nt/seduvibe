d89813bcd45572af3c0aa95ab072c2281239ed15
Tássio <tassiomontenegro@proton.me>
2023-06-13 01:41:34 -0300

    menu att

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -57,7 +57,7 @@ export default {
       items: [
         {
           icon: "mdi-home-outline",
-          url: "/home",
+          url: "/",
           title: "Home",
           active: false,
         },
