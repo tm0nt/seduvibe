07b4b1b823e357baf9718454302f8172e9351248
Tássio <tassiomontenegro@proton.me>
2023-06-16 17:21:47 -0300

    atualizacao

--- src/components/vibeplus/VibePlus.vue
+++ src/components/vibeplus/VibePlus.vue
@@ -1,21 +1,5 @@
 <template>
-  <v-app style="background-color: #212121">
-    <SideBar />
-    <RightSideBar />
-    <Dashboard />
-  </v-app>
+  <v-app style="background-color: #212121"> </v-app>
 </template>
 
-<script>
-import SideBar from "./SideBar.vue";
-import RightSideBar from "./RightSideBar.vue";
-import Dashboard from "./DashboardVie.vue";
-
-export default {
-  components: {
-    SideBar,
-    RightSideBar,
-    Dashboard,
-  },
-};
-</script>
+<script></script>
